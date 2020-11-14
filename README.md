84ecd169228ce524187ac81e3ad963ec91c810b7
Roger Steve <roxhersbozali@gmail.com>
2020-11-13 22:12:17 -0500

    Third commit

--- README.md
+++ README.md
@@ -1,11 +1,12 @@
-# HW-12-MySQL-Employee-Tracker
+# HW-12-Employee-Tracker
+![License](https://img.shields.io/badge/License-APACHE2.0-blue.svg)<br />
 
+
+## Description
 Developers are often tasked with creating interfaces that make it easy for non-developers to view and interact with information stored in databases. 
 Often these interfaces are known as **C**ontent **M**anagement **S**ystems. This application is used to architect and build a solution for managing a company's employees using node, inquirer, and MySQL.
 
-## Description
-
-This is a command-line application that allows users to:
+Thus, this is a command-line application that allows users to:
 
 * Add departments, roles, employees
 
@@ -15,10 +16,22 @@ This is a command-line application that allows users to:
 
 ## User Stories
 
-As a business owner
-I want to be able to view and manage the departments, roles, and employees in my company
-So that I can organize and plan my business
-```
+* As a business owner
+* I want to be able to view and manage the departments, roles, and employees in my company
+* So that I can organize and plan my business
 
-- - -
+
+## Technologies
+ * Javascript
+ * node.js
+ * MySQL 
+ * Enquirer 
+
+## Usage
+  * Run seed file in MySQL Workbench or an equivalent if you would like starting data.
+  * Run node index.js in your terminal to start the application.
+
+## Contributors
+  Roxhers Bozali
+
 © 2019 Trilogy Education Services, a 2U, Inc. brand. All Rights Reserved.
--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+
+//packages
 const mysql = require("mysql");
 const inquirer = require("inquirer");
 require("console.table");
@@ -5,7 +7,7 @@ require("console.table");
 var connection = mysql.createConnection({
     host: "localhost",
 
-    // Your port; if not 3306
+    // Your port; 
     port: 3306,
 
     // Your username
@@ -34,6 +36,7 @@ function start() {
             "add department",
             "add role",
             "update employee role",
+            "remove employee",
             "QUIT"   
         ],
         name:"answers"
@@ -52,8 +55,8 @@ function start() {
             case "add department":
                 addDepartment();
                 break;    
-            case "Remove Employees":
-                removeEmployees();
+            case "remove employee":
+                removeEmployee();
                 break;
             case "add role":
                 addRole();
@@ -87,7 +90,7 @@ function displayEmployees() {
 }
 
 function displayDept() {
-    console.log("Viewing employees by department\n");
+    console.log("Viewing a list of departments\n");
   
     connection.query("SELECT * FROM department ", function (err, res) {
       if (err) throw err;
@@ -100,7 +103,7 @@ function displayDept() {
   }
 
   function addEmployee() {
-
+    console.log("Adding an new Employee\n");
     inquirer
       .prompt([
         {
@@ -129,13 +132,14 @@ function displayDept() {
           if (err) throw err;
   
           console.table(res);
-  
+          console.log("A new Employee was added!\n");
           start();
         });
       });
   }
 
   function addDepartment() {
+    console.log("Adding a new Department!\n");
     inquirer.prompt([{
         type: "input",
         name: "department",
@@ -145,12 +149,62 @@ function displayDept() {
         connection.query('INSERT INTO department (name) VALUES (?)', [res.department], function(err, res) {
             if (err) throw err;
             console.table(res);
+            console.log("A new Department was added!\n");
             start();
         });
     });
   }
 
+  function removeEmployee() {
+    console.log("Deleting an employee");
+
+  var query =
+    `SELECT e.id, e.first_name, e.last_name
+      FROM employee e`
+
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+
+    const deleteEmployeeChoices = res.map(({ id, first_name, last_name }) => ({
+      value: id, name: `${id} ${first_name} ${last_name}`
+    }));
+
+    console.table(res);
+    console.log("ArrayToDelete!\n");
+
+    promptDelete(deleteEmployeeChoices);
+  });
+  }
+
+  function promptDelete(deleteEmployeeChoices) {
+
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          name: "employeeId",
+          message: "Which employee do you want to remove?",
+          choices: deleteEmployeeChoices
+        }
+      ])
+      .then(function (answer) {
+  
+        var query = `DELETE FROM employee WHERE ?`;
+        // when finished prompting, insert a new item into the db with that info
+        connection.query(query, { id: answer.employeeId }, function (err, res) {
+          if (err) throw err;
+  
+          console.table(res);
+          console.log(res.affectedRows + "Deleted!\n");
+  
+          start();
+        });
+        // console.log(query.sql);
+      });
+  }
+
   function addRole() {
+    console.log("Adding a new role!\n");
     inquirer.prompt([
         {
             message: "enter title:",
@@ -166,9 +220,10 @@ function displayDept() {
             name: "department_id"
         }
     ]).then(function (res) {
-        connection.query("INSERT INTO roles (title, salary, department_id) values (?, ?, ?)", [res.title, res.salary, res.department_id], function (err, res) {
+        connection.query("INSERT INTO role (title, salary, department_id) values (?, ?, ?)", [res.title, res.salary, res.department_id], function (err, res) {
           if (err) throw err;
           console.table(res);
+          console.log("A new role was added!\n");
           start();
         });
     });
@@ -176,6 +231,7 @@ function displayDept() {
 }
 
 function updateEmployeeRole() {
+    console.log("Updating an Employee!\n");
   inquirer.prompt([
       {
           message: "which employee would you like to update? (use first name only for now)",
@@ -190,6 +246,7 @@ function updateEmployeeRole() {
       connection.query("UPDATE employee SET role_id = ? WHERE first_name = ?", [res.role_id, res.name], function (err, res) {
           if (err) throw err;
           console.table(res);
+          console.log("Employee has been updated!\n");
           start();
       });
   });
@@ -197,9 +254,11 @@ function updateEmployeeRole() {
 }
 
 function viewAllRoles(){
+    console.log("Viewing Roles!\n");
   connection.query("SELECT role.*, department.name FROM role LEFT JOIN department ON department.id = role.department_id", function (err,res){
     if (err) throw err;
     console.table(res);
+    console.log("Roles are displayed!\n");
     start();
   }
   )};
